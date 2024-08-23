4fa22d7b6006180aa9a8c2b4b56a17b3b27f1ace
Symeon Giazitzis <simosgiaz@gmail.com>
2024-08-23 19:08:59 +0300

    Added the routes for each type of request on both resource classes. Still getting errors, need some more debugging and studying in order to solve.

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 import os.path, csv
 
-rqp = reqparse.RequestParser()                                              #Define request parser values and their properties
-rqp.add_argument('id', type=int)
-rqp.add_argument('name', type=str)
-rqp.add_argument('mpg', type=float)
-rqp.add_argument('cylinders', type=int)
-rqp.add_argument('displacement', type=float)
-rqp.add_argument('horsepower', type=float)
-rqp.add_argument('weight', type=float)
-rqp.add_argument('acceleration', type=float)
-rqp.add_argument('model_year', type=int)
-rqp.add_argument('origin', type=str)
+base_rqp = reqparse.RequestParser()                                         #Define request parser values and their properties
+base_rqp.add_argument('id', type=int)                                       #for basic entry handling in the database
+base_rqp.add_argument('name', type=str)
+base_rqp.add_argument('mpg', type=float)
+base_rqp.add_argument('cylinders', type=int)
+base_rqp.add_argument('displacement', type=float)
+base_rqp.add_argument('horsepower', type=float)
+base_rqp.add_argument('weight', type=float)
+base_rqp.add_argument('acceleration', type=float)
+base_rqp.add_argument('model_year', type=int)
+base_rqp.add_argument('origin', type=str)
 
-drqp = reqparse.RequestParser()                                             #Define request parser values for deleting entries
-drqp.add_argument('id', type=int, required=True, help='Id is required in order to delete a car model.')
+del_rqp = reqparse.RequestParser()                                          #Define request parser values for deleting entries
+del_rqp.add_argument('id', type=int, required=True, help='Id is required in order to delete a car model.')
+
+year_rqp = reqparse.RequestParser()
+year_rqp.add_argument('model_year_1', type=int, required=True, help='Two year dates are required to search in year range')
+year_rqp.add_argument('model_year_2', type=int, required=True, help='Two year dates are required to search in year range')
 
 app = Flask(__name__)                                                       #Initialize the flask application for the API
 api = Api(app)
@@ -75,9 +79,10 @@ resource_fields = {                                                         #Def
 
 class Car_Brands(Resource):
 
+    @app.route('/get_models', methods=['GET'])                                     #Define the flask request route 
     @marshal_with(resource_fields)                                          #Marshal the return with the defined resource fields
-    def get(self):                                                          #Find and return the searched entries
-        filtered_args = {k: v for k, v in rqp.parse_args().items() if v}    #Filter the arguments given by the user so that 
+    def get_model(self):                                                         #Find and return the searched entries
+        filtered_args = {k: v for k, v in base_rqp.parse_args().items() if v}    #Filter the arguments given by the user so that 
                                                                             #there are no empty values
         result = CarModel.query.filter_by(**filtered_args).all()            #Filter the database with the given arguments
 
@@ -86,50 +91,63 @@ class Car_Brands(Resource):
 
         return result
     
-    @marshal_with(resource_fields)                                          
-    def post(self):
-        filtered_args = {k: v for k, v in rqp.parse_args().items() if v}    
-        result = CarModel.query.filter_by(**filtered_args).all() 
+    # @app.route('/post_entry', methods=['POST'])
+    # @marshal_with(resource_fields)                                          
+    # def post_new_entries(self):
+    #     filtered_args = {k: v for k, v in base_rqp.parse_args().items() if v}    
+    #     result = CarModel.query.filter_by(**filtered_args).all() 
 
-        if not result:                                                      #Check if there are any entries in the database with 
-                                                                            #the exact same attributes with the ones given by the user
-            try:                                                            #Try to insert the new object in the database
-                with app.app_context():
-                    model = CarModel(**filtered_args)
-                    db.session.add(model)
-                    db.session.commit()
-            except IntegrityError as x:                                     #Catch integrity errors
-                print("error:", x)
-                abort(409, description=x)
-            except:                                                         #Catch any other errors
-                abort(500, description="There was an error with your request.")
-        else:                                                                   
-            abort(409, description="A car with the same characteristics already exist.")    #In case there is another entry
-                                                                                            #very similar to the one given, abort
+    #     if not result:                                                      #Check if there are any entries in the database with 
+    #                                                                         #the exact same attributes with the ones given by the user
+    #         try:                                                            #Try to insert the new object in the database
+    #             with app.app_context():
+    #                 model = CarModel(**filtered_args)
+    #                 db.session.add(model)
+    #                 db.session.commit()
+    #         except IntegrityError as x:                                     #Catch integrity errors
+    #             print("error:", x)
+    #             abort(409, description=x)
+    #         except:                                                         #Catch any other errors
+    #             abort(500, description="There was an error with your request.")
+    #     else:                                                                   
+    #         abort(409, description="A car with the same characteristics already exist.")    #In case there is another entry
+    #                                                                                         #very similar to the one given, abort
 
-        return {**filtered_args, "message": "Car model added successfully!"}
+    #     return {**filtered_args, "message": "Car model added successfully!"}
     
-    @marshal_with(resource_fields)
-    def delete(self):                                                       #Delete the entry matching the given id
-        filtered_args = {k: v for k, v in drqp.parse_args().items() if v}   #Filter the arguments given by the user so that there 
-                                                                            #are no empty values
+    # @app.route('/delete', methods=['DELETE'])
+    # @marshal_with(resource_fields)
+    # def delete(self):                                                     #Delete the entry matching the given id
+    #     filtered_args = {k: v for k, v in del_rqp.parse_args().items() if v}    #Filter the arguments given by the user so that there 
+    #                                                                             #are no empty values
         
-        try:
-            with app.app_context():
-                result = db.session.get(CarModel, filtered_args['id'])            #Get the object with the specified id given by the user
-                db.session.delete(result)                                   #Delete the entry from the database
-                db.session.commit()
-        except:
-            abort(500, description="There was an error deleting a car model.")
+    #     try:
+    #         with app.app_context():
+    #             result = db.session.get(CarModel, filtered_args['id'])      #Get the object with the specified id given by the user
+    #             db.session.delete(result)                                   #Delete the entry from the database
+    #             db.session.commit()
+    #     except:
+    #         abort(500, description="There was an error deleting a car model.")
 
-                                                                            #Get the values from the result object
-        result_val = {column.name: getattr(result, column.name) for column in result.__table__.columns} 
+    #                                                                         #Get the values from the result object
+    #     result_val = {column.name: getattr(result, column.name) for column in result.__table__.columns} 
         
-        return {'message': 'Car model deleted successfully!', **result_val}
+    #     return {'message': 'Car model deleted successfully!', **result_val}
 
-        
-    
-api.add_resource(Car_Brands, "/Car_Brands")    #<int:car_id>                Add a resource to the API
+# class Year_Comparison(Resource):
+
+#     @app.route('/get_year_range', methods=['GET'])
+#     @marshal_with(resource_fields)
+#     def get_year_range(self):
+
+#         args = year_rqp.parse_args()
+#         result = db.session().query(CarModel).filter(CarModel.model_year.between(args['model_year_1'], args['model_year_2'])).all()
+
+#         return result
+
+                                                                                            #Add a resource to the API
+api.add_resource(Car_Brands, "/Car_Brands/get_models", "/Car_Brands/delete", "/Car_Brands/post_entry")   #<int:car_id> 
+# api.add_resource(Year_Comparison, "/Year_Comparison/get_year_range")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,8 +2,39 @@ import requests as rq
 
 BASE = "http://127.0.0.1:5000/"
 
-response = rq.delete(url=BASE + "Car_Brands", 
-                  json={'id': 505})
+# rqs= {
+#     0 : {'id': 510, 'name': 'Seat Ibiza', 'year': 2010, 'cylinders': 4, 'origin': 'germany', 'weight': 1200, 'horsepower': 70},
+#     1 : {'displacement': 1997, 'name': 'bmw m3', 'cylinders': 6, 'acceleration': 7.5, 'weight': 1600, 'horsepower': 170},
+#     2 : {'name': 'VW Polo', 'year': 2004, 'cylinders': 3, 'origin': 'germany', 'weight': 900, 'horsepower': 67}
+# }
+# for i in range(3):
+#     response = rq.post(url=BASE + "Car_Brands/post", json=rqs[i])
+
+#     if response.status_code == 200:
+#         data = response.json()
+#         print(data)
+#     else:
+#         print(f"Error: {response.status_code} - {response.text}")
+
+# response = rq.get(url=BASE + "Car_Brands/get", json=rqs[2])
+
+# if response.status_code == 200:
+#     data = response.json()
+#     print(data)
+# else:
+#     print(f"Error: {response.status_code} - {response.text}")
+
+# response = rq.delete(url=BASE + "Car_Brands/delete", json={'id':512})
+
+# if response.status_code == 200:
+#     data = response.json()
+#     print(data)
+# else:
+#     print(f"Error: {response.status_code} - {response.text}")
+
+# response = rq.get(url=BASE + "Year_Comparison/get_year_range", json={'model_year_1': 75, 'model_year_2': 76})
+response = rq.get(url=BASE + "Car_Brands/get_models", json={'name': 'VW Polo', 'year': 2004, 
+                                                     'cylinders': 3, 'origin': 'germany', 'weight': 900, 'horsepower': 67})
 
 if response.status_code == 200:
     data = response.json()
